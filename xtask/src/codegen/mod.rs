d92e5d6e3e1cc83f5a0c28a8844de4854c433361
manapointer <manapointer@gmail.com>
2022-12-21 10:58:18 -0800

    continue codegen

--- xtask/src/codegen/mod.rs
+++ xtask/src/codegen/mod.rs
@@ -1,8 +1,34 @@
-use std::mem;
+use std::{
+    collections::HashMap,
+    env, mem,
+    path::{Path, PathBuf},
+};
 
 #[derive(Default, Debug)]
 struct CommentBlock {
     lines: Vec<String>,
+}
+
+#[derive(Debug)]
+struct Test {
+    name: String,
+    text: String,
+}
+
+impl Test {
+    fn new(name: String, text: String) -> Test {
+        Test { name, text }
+    }
+}
+
+fn project_root() -> PathBuf {
+    Path::new(
+        &env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_string()),
+    )
+    .ancestors()
+    .nth(1)
+    .unwrap()
+    .to_path_buf()
 }
 
 fn extract_comment_blocks(text: &str) -> Vec<CommentBlock> {
@@ -26,16 +52,50 @@ fn extract_comment_blocks(text: &str) -> Vec<CommentBlock> {
     blocks
 }
 
+fn add_tests_from_comment_blocks(tests: &mut HashMap<String, Test>, blocks: &[CommentBlock]) {
+    for block in blocks {
+        if block.lines.is_empty() {
+            continue;
+        }
+        let header = block.lines[0].trim_start();
+        let name = {
+            let mut parts = header.split_ascii_whitespace();
+            if parts.next() != Some("test") {
+                continue;
+            }
+            match parts.next() {
+                Some(name) => name,
+                None => continue,
+            }
+        };
+
+        let text = block.lines[1..].join("\n");
+        if !text.is_empty() {
+            tests.insert(name.to_string(), Test::new(name.to_string(), text));
+        }
+    }
+}
+
 pub fn run() -> Result<(), anyhow::Error> {
+    let mut tests: HashMap<String, Test> = HashMap::new();
+
+    // For each .rs file in crates/star_syntax/src/parser
+    //   Parse all blocks
+
     let text = "
     
-// block1
-// block1
+// x + y
 
-// block2
-// block2";
+// test def_stmt
+// def foo():
+//   pass";
 
-    println!("{:?}", extract_comment_blocks(text));
+    // println!("writing to {}", project_root().display());
+    let blocks = extract_comment_blocks(text);
+
+    add_tests_from_comment_blocks(&mut tests, &blocks);
+
+    eprintln!("{:?}", tests);
 
     Ok(())
 }
