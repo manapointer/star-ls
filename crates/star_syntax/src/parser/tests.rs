0d1fb5028b4056426e27b89665d3fed4a48362a9
manapointer <manapointer@gmail.com>
2022-12-30 19:01:09 -0800

    allow filtering parser tests

--- crates/star_syntax/src/parser/tests.rs
+++ crates/star_syntax/src/parser/tests.rs
@@ -17,45 +17,58 @@ fn project_root() -> PathBuf {
     .to_path_buf()
 }
 
-fn collect_star_files(test_dir: &Path) -> Result<Vec<(PathBuf, String, bool)>, io::Error> {
+fn collect_star_files(
+    test_dir: &Path,
+    filters: &[&str],
+) -> Result<Vec<(PathBuf, String)>, io::Error> {
     let mut res = Vec::new();
 
     let ok_dir = test_dir.join("ok");
     let err_dir = test_dir.join("err");
-    for entry in fs::read_dir(&ok_dir)? {
+
+    for entry in fs::read_dir(&ok_dir)?.chain(fs::read_dir(&err_dir)?) {
         let entry = entry?;
         let path = entry.path();
         if path.extension().unwrap_or_default() != "star" || !entry.file_type()?.is_file() {
             continue;
         }
-        let code = fs::read_to_string(&path)?;
-        res.push((path, code, true));
-    }
-    for entry in fs::read_dir(&err_dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.extension().unwrap_or_default() != "star" || !entry.file_type()?.is_file() {
+        // Filter tests.
+        if !filters.is_empty()
+            && filters.iter().all(|&filter| {
+                !path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .starts_with(filter)
+            })
+        {
             continue;
         }
         let code = fs::read_to_string(&path)?;
-        res.push((path, code, false));
+        res.push((path, code));
     }
     Ok(res)
 }
 
 #[test]
 fn dir_tests() {
-    let star_files =
-        collect_star_files(&project_root().join("crates/star_syntax/src/parser/test_data"))
-            .unwrap();
+    let filter_str = env::var("STAR_PARSER_TESTS").unwrap_or_else(|_| String::new());
+    let filters: Vec<&str> = filter_str.split(',').map(str::trim).collect();
 
-    for (path, input, is_ok) in star_files {
+    let star_files = collect_star_files(
+        &project_root().join("crates/star_syntax/src/parser/test_data"),
+        &filters,
+    )
+    .unwrap();
+
+    for (path, input) in star_files {
         let ast_path = path.with_extension("star.ast");
-        check(&input, ast_path, is_ok);
+        check(&input, ast_path);
     }
 }
 
-fn check(input: &str, expect_path: PathBuf, is_ok: bool) {
+fn check(input: &str, expect_path: PathBuf) {
     let parse = parse_file(input);
     let rendered = render(parse.syntax(), parse.errors);
     let expect = expect_file![expect_path];
